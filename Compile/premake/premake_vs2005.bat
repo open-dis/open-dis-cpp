80a92c92c530e6774ef22e25ac451c069f561916
Leif Gruenwoldt <leifer@gmail.com>
2017-07-20 11:06:35 -0400

    Remove Compile dir
    These are pretty old and can be generated with premake.
    The premake files themselves don't work with modern premake.
    So see the new premake5.lua file in the project root instead.

--- Compile/premake/premake_vs2005.bat
+++
@@ -1,8 +0,0 @@
-pushd Projects
-rem set DIS_LIB_INSTALL="where you want the build system to send the binary library files to"
-rem set DIS_BIN_INSTALL="where you want the build system to send the other binary files to"premake --file DIS.lua --target vs2005 
-rem set HAWKNL_INC_DIR="where the headers for HawkNL exist."
-rem set HAWKNL_LIB_DIR="where the library for HawkNL exists."
-premake --file DIS.lua --target vs2005
-popd
-pause
