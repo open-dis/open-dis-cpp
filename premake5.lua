bb651a502bcd925a2448ad2c63512f8d8a57c179
Leif Gruenwoldt <leifer@gmail.com>
2017-07-20 09:53:33 -0400

    Create premake5.lua
    The premake files in Compile/premake seem to be for an old version of premake that's no longer supported?

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+--
+-- premake5 file to build open-dis-cpp
+-- http://premake.github.io/
+--
+
+workspace "open-dis-cpp"
+	configurations { 
+		"Debug",
+		"Release"
+	}
+	targetdir "Build/lib/%{cfg.buildcfg}"
+
+	configuration "Debug*"
+		defines { "DEBUG", }
+    symbols "On"
+ 
+	configuration "Release*"
+		defines { "NDEBUG" }
+		optimize "On"
+
+	-- windows specific
+	configuration "windows"
+		defines { "WIN32" }
+
+	-- linux specific
+	configuration { "linux", "gmake" }
+    buildoptions {
+			"-Wall",
+			"-Werror"
+	  }
+
+project "OpenDIS"
+	language "C++"
+	kind "StaticLib"
+	includedirs { 
+		"cpp/DIS",
+    "CPPUtils/DIS"
+	}
+	files { 
+    "cpp/DIS/*.h",
+    "cpp/DIS/*.cpp",
+    "CPPUtils/DIS/*.h"
+    "CPPUtils/DIS/*.cpp"
+	}
