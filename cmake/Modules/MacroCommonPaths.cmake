478cc21e8c051bc71eea7d547d6f99eb76769dbd
Tara Madden <tara.madden@gtri.gatech.edu>
2019-12-06 16:27:53 -0500

    removed references to scrimmage/SCRIMMAGE from all files where it was present

--- cmake/Modules/MacroCommonPaths.cmake
+++ cmake/Modules/MacroCommonPaths.cmake
@@ -1,7 +1,6 @@
 macro(MacroCommonPaths NAME DIR_NAME)
   IF(DEFINED ENV{${NAME}_ROOT} OR DEFINED ENV{${NAME}_DIR})
     set(COMMON_INCLUDE_PATHS_${NAME}
-      /opt/scrimmage/include/JSBSim
       $ENV{${NAME}_DIR}/src
       $ENV{${NAME}_DIR}/include
       $ENV{${NAME}_DIR}
@@ -11,7 +10,6 @@ macro(MacroCommonPaths NAME DIR_NAME)
       )
   ELSE()
     set(COMMON_INCLUDE_PATHS_${NAME}
-      /opt/scrimmage/include
       ~/Library/Frameworks
       /Library/Frameworks
       /usr/local/include
@@ -39,7 +37,6 @@ macro(MacroCommonPaths NAME DIR_NAME)
       )
   ELSE()
     set(COMMON_LIBRARY_PATHS_${NAME}
-      /opt/scrimmage/lib
       ~/Library/Frameworks
       /Library/Frameworks
       /usr/local/lib
@@ -59,7 +56,6 @@ macro(MacroCommonPaths NAME DIR_NAME)
     $ENV{${NAME}_DIR}
     $ENV{${NAME}_ROOT}/share
     $ENV{${NAME}_ROOT}
-    /opt/scrimmage/etc
     ~/Library/Frameworks
     /Library/Frameworks
     /usr/local/share
