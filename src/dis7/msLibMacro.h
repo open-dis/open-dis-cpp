da4835282c0beb78a0d749d0d75b46fe3874c905
MATTHEW HUNKAPILLER <wade.hunkapiller@ams3sx.com>
2023-01-27 08:56:46 -0600

    Tweak library macro

--- src/dis7/msLibMacro.h
+++ src/dis7/msLibMacro.h
@@ -3,10 +3,12 @@
 #  define EXPORT_MACRO
 #else
 #  if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
-#    ifdef OpenDIS7_EXPORTS
-#      define EXPORT_MACRO __declspec(dllexport)
-#    else
-#      define EXPORT_MACRO  __declspec(dllimport)
+#    ifndef EXPORT_MACRO
+#      ifdef OpenDIS7_EXPORTS
+#        define EXPORT_MACRO __declspec(dllexport)
+#      else
+#        define EXPORT_MACRO  __declspec(dllimport)
+#      endif
 #    endif
 #  else
 #    define EXPORT_MACRO
