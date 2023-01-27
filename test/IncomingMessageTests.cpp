3aa67b4484515265edc62f0eafac0d719312ea63
Wade Hunkapiller <wade.hunk@gmail.com>
2023-01-27 09:33:51 -0600

    Resurrect unit tests using catch

--- test/IncomingMessageTests.cpp
+++ test/IncomingMessageTests.cpp
@@ -1,210 +1,163 @@
-/// Copyright goes here
-/// License goes here
-/// @author John K. Grant
+#include "catch.hpp"
 
-#include <cppunit/extensions/HelperMacros.h>
-
-#include <DIS/IncomingMessage.h>    // for testing
-#include <DIS/DataStream.h>         // for usage
-#include <DIS/IPacketProcessor.h>   // for usage
-#include <DIS/EntityStatePdu.h>     // for usage
-#include <DIS/DetonationPdu.h>     // for usage
-#include <DIS/CollisionPdu.h>     // for usage
-#include <DIS/PDUType.h>
 #include "PduUtils.h"
+#include "dis6/CollisionPdu.h"
+#include "dis6/DetonationPdu.h"
+#include "dis6/EntityStatePdu.h"
+#include "utils/DataStream.h"
+#include "utils/IPacketProcessor.h"
+#include "utils/IncomingMessage.h"
+#include "utils/PDUType.h"
 
-namespace TestDIS
-{
-   /// tests the support for listening to packets
-   class IMTests : public CPPUNIT_NS::TestFixture
-   {
-   public:
-      void setup();
-      void teardown();
-
-      void TestAddRemoveProcessor();
-      void TestObserving();
-
-      void TestEntityStateThenDetonation();
-      void TestCollisionThenEntityState();
-
-      CPPUNIT_TEST_SUITE( IMTests );
-         CPPUNIT_TEST( TestAddRemoveProcessor );
-         CPPUNIT_TEST( TestObserving );
-         CPPUNIT_TEST( TestEntityStateThenDetonation );
-         CPPUNIT_TEST( TestCollisionThenEntityState );
-      CPPUNIT_TEST_SUITE_END();
-
-   protected:
-      template<typename PduT1, typename PduT2>
-      void TestMultiplePackets(const PduT1& src1, const PduT2& src2);
-   };
-
-   /// knows if it has been visited
-   class HitProcessor : public DIS::IPacketProcessor
-   {
-   private:
-      DIS::PDUType mPduType;
-
-   public:
-      unsigned int _hits;
-
-      HitProcessor(DIS::PDUType pdut)
-         : DIS::IPacketProcessor()
-         , mPduType(pdut)
-         , _hits(0)
-      {
-      }
-
-      DIS::PDUType GetRegisteredType() const { return mPduType; }
-
-      /// test casting to a concrete type
-      void Process(const DIS::Pdu& packet)
-      {
-         //const DIS::EntityStatePdu& espdu = static_cast<const DIS::EntityStatePdu&>( packet );
-         //espdu.getEntityAppearance();
-         _hits++;
-      }
-
-   private:
-      HitProcessor(); ///< not implemented by design
-   };
-}
-
-using namespace TestDIS;
 using namespace DIS;
-CPPUNIT_TEST_SUITE_REGISTRATION( IMTests );
 
+/// knows if it has been visited
+class HitProcessor : public IPacketProcessor {
+ private:
+  PDUType mPduType;
 
-void IMTests::TestAddRemoveProcessor()
-{
-   EntityStatePdu espdu;
-   HitProcessor hp0((DIS::PDUType)espdu.getPduType());
+ public:
+  unsigned int _hits;
 
-   IncomingMessage im;
-   CPPUNIT_ASSERT( im.GetProcessors().empty() );
+  HitProcessor(PDUType pdut) : IPacketProcessor(), mPduType(pdut), _hits(0) {}
 
-   // test a single mapping
-   im.AddProcessor( hp0.GetRegisteredType() , &hp0 );
-   CPPUNIT_ASSERT_EQUAL( im.GetProcessors().size(), size_t(1) );
+  PDUType GetRegisteredType() const { return mPduType; }
 
-   im.RemoveProcessor( hp0.GetRegisteredType() , &hp0 );
-   CPPUNIT_ASSERT_EQUAL( im.GetProcessors().size(), size_t(0) );
-   CPPUNIT_ASSERT( im.GetProcessors().empty() );
+  /// test casting to a concrete type
+  void Process(const Pdu& packet) {
+    // const EntityStatePdu& espdu = static_cast<const
+    // EntityStatePdu&>( packet ); espdu.getEntityAppearance();
+    _hits++;
+  }
 
-   // test the multimap
-   DIS::CollisionPdu cpdu;
-   HitProcessor hp1((DIS::PDUType)cpdu.getPduType());
+ private:
+  HitProcessor();  ///< not implemented by design
+};
 
-   im.AddProcessor( hp0.GetRegisteredType() , &hp0 );
-   CPPUNIT_ASSERT_EQUAL( im.GetProcessors().size(), size_t(1) );
+template <typename PduT1, typename PduT2>
+void TestMultiplePackets(const PduT1& src1, const PduT2& src2) {
+  // populate the factory
+  IncomingMessage im;
 
-   im.AddProcessor( hp0.GetRegisteredType() , &hp1 );
-   CPPUNIT_ASSERT_EQUAL( im.GetProcessors().size(), size_t(2) );
+  // sample PDUs for testing
+  const unsigned int NUM_ES = 3;
+  PduT1 pdu1_array[NUM_ES];
+  for (unsigned int pdu1clone = 0; pdu1clone < NUM_ES; ++pdu1clone) {
+    pdu1_array[pdu1clone] = src1;
+  }
+  PduT2 pdu2 = src2;
 
-   // This should not allow multiples of the exact same pair
-   im.AddProcessor( hp0.GetRegisteredType() , &hp0 );
-   CPPUNIT_ASSERT_EQUAL( im.GetProcessors().size(), size_t(2) );
+  // will tell us if the packet processor framework within IncomingMessage is
+  // working.
+  HitProcessor hp_es((PDUType)pdu1_array[0].getPduType());
+  HitProcessor hp_dt((PDUType)pdu2.getPduType());
+  REQUIRE(hp_es._hits == (unsigned int)0);  // better not be hit yet
+  REQUIRE(hp_dt._hits == (unsigned int)0);  // better not be hit yet
 
-   // this should not remove the pair that does not exist
-   im.RemoveProcessor( hp1.GetRegisteredType() , &hp0 );
-   CPPUNIT_ASSERT_EQUAL( im.GetProcessors().size(), size_t(2) );
+  // map the HitProcessor to the type of pdu
+  im.AddProcessor(hp_es.GetRegisteredType(), &hp_es);
+  im.AddProcessor(hp_dt.GetRegisteredType(), &hp_dt);
 
-   im.RemoveProcessor( hp0.GetRegisteredType() , &hp1 );
-   CPPUNIT_ASSERT_EQUAL( im.GetProcessors().size(), size_t(1) );
+  // fake a network buffer to read
+  DataStream ds(BIG);
+  for (unsigned int pdu1clone = 0; pdu1clone < NUM_ES; ++pdu1clone) {
+    pdu1_array[pdu1clone].marshal(ds);
+  }
+  pdu2.marshal(ds);
 
-   im.RemoveProcessor( hp0.GetRegisteredType() , &hp0 );
-   CPPUNIT_ASSERT_EQUAL( im.GetProcessors().size(), size_t(0) );
-   CPPUNIT_ASSERT( im.GetProcessors().empty() );
+  // test the packet processor framework
+  im.Process(&(ds[0]), ds.size(), ds.GetStreamEndian());
+  REQUIRE(hp_es._hits == NUM_ES);  // better be hit for all the ESPdu only
+  REQUIRE(hp_dt._hits ==
+          (unsigned int)1);  // better be hit for all the ESPdu only
+
+  // clean up things
+  im.RemoveProcessor(hp_es.GetRegisteredType(), &hp_es);
+  im.RemoveProcessor(hp_dt.GetRegisteredType(), &hp_dt);
 }
 
-void IMTests::TestObserving()
-{
-   // sample pdu for testing
-   EntityStatePdu espdu;
+TEST_CASE("Add/remove processor", "[IncomingMessage]") {
+  EntityStatePdu espdu;
+  HitProcessor hp0((PDUType)espdu.getPduType());
 
-   // will tell us if the packet processor framework within IncomingMessage is working.
-   HitProcessor hp((DIS::PDUType)espdu.getPduType());
-   CPPUNIT_ASSERT_EQUAL( hp._hits, (unsigned int)0 );  // better not be hit
+  IncomingMessage im;
+  REQUIRE(im.GetProcessors().empty());
 
-   // map the HitProcessor to the type of pdu
-   IncomingMessage im;
-   im.AddProcessor( espdu.getPduType() , &hp );
+  // test a single mapping
+  im.AddProcessor(hp0.GetRegisteredType(), &hp0);
+  REQUIRE(im.GetProcessors().size() == size_t(1));
 
-   // fake a network buffer to read
-   DIS::DataStream ds(DIS::BIG);
-   espdu.marshal( ds );
+  im.RemoveProcessor(hp0.GetRegisteredType(), &hp0);
+  REQUIRE(im.GetProcessors().size() == size_t(0));
+  REQUIRE(im.GetProcessors().empty());
 
-   // test the packet processor framework
-   im.Process( &(ds[0]), ds.size(), ds.GetStreamEndian() );
-   CPPUNIT_ASSERT_EQUAL( hp._hits, (unsigned int)1 );  // better be hit now
+  // test the multimap
+  CollisionPdu cpdu;
+  HitProcessor hp1((PDUType)cpdu.getPduType());
 
-   // clean up things
-   im.RemoveProcessor( espdu.getPduType() , &hp );
+  im.AddProcessor(hp0.GetRegisteredType(), &hp0);
+  REQUIRE(im.GetProcessors().size() == size_t(1));
+
+  im.AddProcessor(hp0.GetRegisteredType(), &hp1);
+  REQUIRE(im.GetProcessors().size() == size_t(2));
+
+  // This should not allow multiples of the exact same pair
+  im.AddProcessor(hp0.GetRegisteredType(), &hp0);
+  REQUIRE(im.GetProcessors().size() == size_t(2));
+
+  // this should not remove the pair that does not exist
+  im.RemoveProcessor(hp1.GetRegisteredType(), &hp0);
+  REQUIRE(im.GetProcessors().size() == size_t(2));
+
+  im.RemoveProcessor(hp0.GetRegisteredType(), &hp1);
+  REQUIRE(im.GetProcessors().size() == size_t(1));
+
+  im.RemoveProcessor(hp0.GetRegisteredType(), &hp0);
+  REQUIRE(im.GetProcessors().size() == size_t(0));
+  REQUIRE(im.GetProcessors().empty());
 }
 
-void IMTests::TestEntityStateThenDetonation()
-{
-   DIS::EntityStatePdu espdu;
-   TestDIS::InitPDU( espdu );
+TEST_CASE("Observing", "[IncomingMessage]") {
+  // sample pdu for testing
+  EntityStatePdu espdu;
 
-   DIS::DetonationPdu detpdu;
-   TestDIS::InitPDU( detpdu );
+  // will tell us if the packet processor framework within IncomingMessage is
+  // working.
+  HitProcessor hp((PDUType)espdu.getPduType());
+  REQUIRE(hp._hits == (unsigned int)0);  // better not be hit
 
-   TestMultiplePackets( espdu , detpdu );
+  // map the HitProcessor to the type of pdu
+  IncomingMessage im;
+  im.AddProcessor(espdu.getPduType(), &hp);
+
+  // fake a network buffer to read
+  DataStream ds(BIG);
+  espdu.marshal(ds);
+
+  // test the packet processor framework
+  im.Process(&(ds[0]), ds.size(), ds.GetStreamEndian());
+  REQUIRE(hp._hits == (unsigned int)1);  // better be hit now
+
+  // clean up things
+  im.RemoveProcessor(espdu.getPduType(), &hp);
 }
 
-void IMTests::TestCollisionThenEntityState()
-{
-   DIS::CollisionPdu colpdu;
-   TestDIS::InitPDU( colpdu );
+TEST_CASE("EntityState then Detonation", "[IncomingMessage]") {
+  EntityStatePdu espdu;
+  TestDIS::InitPDU(espdu);
 
-   DIS::EntityStatePdu espdu;
-   TestDIS::InitPDU( espdu );
+  DetonationPdu detpdu;
+  TestDIS::InitPDU(detpdu);
 
-   TestMultiplePackets( colpdu , espdu );
+  TestMultiplePackets(espdu, detpdu);
 }
 
-template<typename PduT1, typename PduT2>
-void IMTests::TestMultiplePackets(const PduT1& src1, const PduT2& src2)
-{
-   // populate the factory
-   IncomingMessage im;
+TEST_CASE("Collision then EntityState", "[IncomingMessage]") {
+  CollisionPdu colpdu;
+  TestDIS::InitPDU(colpdu);
 
-   // sample PDUs for testing
-   const unsigned int NUM_ES = 3;
-   PduT1 pdu1_array[NUM_ES];
-   for(unsigned int pdu1clone=0; pdu1clone<NUM_ES; ++pdu1clone)
-   {
-      pdu1_array[pdu1clone] = src1;
-   }
-   PduT2 pdu2 = src2;
+  EntityStatePdu espdu;
+  TestDIS::InitPDU(espdu);
 
-   // will tell us if the packet processor framework within IncomingMessage is working.
-   HitProcessor hp_es((DIS::PDUType)pdu1_array[0].getPduType());
-   HitProcessor hp_dt((DIS::PDUType)pdu2.getPduType());
-   CPPUNIT_ASSERT_EQUAL( hp_es._hits, (unsigned int)0 );  // better not be hit yet
-   CPPUNIT_ASSERT_EQUAL( hp_dt._hits, (unsigned int)0 );  // better not be hit yet
-
-   // map the HitProcessor to the type of pdu
-   im.AddProcessor( hp_es.GetRegisteredType() , &hp_es );
-   im.AddProcessor( hp_dt.GetRegisteredType() , &hp_dt );
-
-   // fake a network buffer to read
-   DIS::DataStream ds(DIS::BIG);
-   for(unsigned int pdu1clone=0; pdu1clone<NUM_ES; ++pdu1clone)
-   {
-      pdu1_array[pdu1clone].marshal( ds );
-   }
-   pdu2.marshal( ds );
-
-   // test the packet processor framework
-   im.Process( &(ds[0]), ds.size(), ds.GetStreamEndian() );
-   CPPUNIT_ASSERT_EQUAL( hp_es._hits, NUM_ES );  // better be hit for all the ESPdu only
-   CPPUNIT_ASSERT_EQUAL( hp_dt._hits, (unsigned int)1 );  // better be hit for all the ESPdu only
-
-   // clean up things
-   im.RemoveProcessor( hp_es.GetRegisteredType() , &hp_es );
-   im.RemoveProcessor( hp_dt.GetRegisteredType() , &hp_dt );
+  TestMultiplePackets(colpdu, espdu);
 }
-
