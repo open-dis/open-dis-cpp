e55bc0681e4c68d3d85471ae14aa43120c368950
MATTHEW HUNKAPILLER <wade.hunkapiller@ams3sx.com>
2023-01-27 08:28:28 -0600

    Tweak msLibMacro to support static libraries

--- src/dis6/msLibMacro.h
+++ src/dis6/msLibMacro.h
@@ -1,10 +1,14 @@
 #pragma once
-#if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
-#  ifdef EXPORT_LIBRARY
-#    define EXPORT_MACRO __declspec(dllexport)
-#  else
-#    define EXPORT_MACRO  __declspec(dllimport)
-#  endif
-#else
+#ifdef OpenDIS6_STATIC_DEFINE
 #  define EXPORT_MACRO
+#else
+#  if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
+#    ifdef OpenDIS6_EXPORTS
+#      define EXPORT_MACRO __declspec(dllexport)
+#    else
+#      define EXPORT_MACRO  __declspec(dllimport)
+#    endif
+#  else
+#    define EXPORT_MACRO
+#  endif
 #endif
