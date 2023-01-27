c7a74605141f41d3774a21bf9458fe0b90112e35
Wade Hunkapiller <wade.hunk@gmail.com>
2023-01-27 09:22:04 -0600

    Modernize and modularize cmake build system for more robust usage

--- src/dis7/msLibMacro.h
+++ src/dis7/msLibMacro.h
@@ -1,12 +1,16 @@
-#ifndef MSLIBMACRO_H
-#define MSLIBMACRO_H
-#if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
-#  ifdef EXPORT_LIBRARY
-#    define EXPORT_MACRO __declspec(dllexport)
-#  else
-#    define EXPORT_MACRO  __declspec(dllimport)
-#  endif
-#else
+#pragma once
+#ifdef OpenDIS7_STATIC_DEFINE
 #  define EXPORT_MACRO
-#endif
+#else
+#  if defined(_MSC_VER) || defined(__CYGWIN__) || defined(__MINGW32__) || defined( __BCPLUSPLUS__)  || defined( __MWERKS__)
+#    ifndef EXPORT_MACRO
+#      ifdef OpenDIS7_EXPORTS
+#        define EXPORT_MACRO __declspec(dllexport)
+#      else
+#        define EXPORT_MACRO  __declspec(dllimport)
+#      endif
+#    endif
+#  else
+#    define EXPORT_MACRO
+#  endif
 #endif
